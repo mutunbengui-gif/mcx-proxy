623f9d0c7870802d8c8a10834fbea222f8f8054a
mutunbengui-gif <mutu.n.bengui@gmail.com>
2026-04-09 13:13:59 +0100

    Update index.js

--- index.js
+++ index.js
@@ -35,18 +35,42 @@ app.post("/charge", async (req, res) => {
   try {
     const token = req.headers.authorization;
 
+    const payload = {
+      amount: req.body.amount,
+      config: {
+        viewType: "QR_CODE",
+        reference: req.body.reference,
+        size: "LARGE",
+        description: req.body.description || "MCX Payment",
+        type: "DYNAMIC"
+      },
+      posId: req.body.posId || "2096",
+      notify: {
+        callbackUrl: {
+          value: req.body.callbackUrl || "https://webhook.site/8e462f9d-51e3-41a7-a3af-fe40723ba947",
+          active: true
+        },
+        mobile: {
+          value: req.body.mobile || 924061515,
+          active: true
+        },
+        email: {
+          value: req.body.email || "mutu.n.bengui@gmail.com",
+          active: false
+        }
+      }
+    };
+
     const response = await fetch(
       "https://cerpagamentonline.emis.co.ao/online-payment-gateway/api/v1/merchants/1275/charges",
       {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
-          "Accept": "text/plain",
-          "Authorization": token?.startsWith("Bearer ")
-            ? token
-            : `Bearer ${token}`
+          "Authorization": token,
+          "Accept": "application/json"
         },
-        body: JSON.stringify(req.body)
+        body: JSON.stringify(payload)
       }
     );
 
